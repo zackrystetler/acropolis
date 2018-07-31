a2c209593741fa37d3c563f2aea5bd49ad275bdd
Zackry Stetler <zackstetler@gmail.com>
2018-07-31 21:26:54 +1200

    Responsive Layout Complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 //ig client id=7fcf52f7bdc046bdadad9fdd9b735f25
+
+//smooth scrolling to anchors
+
+//header
+//duplicate nav
+//remove class mainNav
+//add class sideNav
+//add close button
+//perform slide in and slide out
+
+//clean up map (remove ui)
+
+//menu funcktionality
+
+//instafeed
